f699c962b7d644542bfd6c43506d67fb4e71d6af
Greeshma-G-Nair <97226862+Greeshma-G-Nair@users.noreply.github.com>
2022-01-22 22:44:52 +0530

    Create fibonacci.java

---
+++ cycle 1/fibonacci.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class fibonacci
+{
+  public static void main(String args[])
+   {
+     System.out.println("Enter the value of n");
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     
+     int i,sum=0,a=0,b=1;
+   
+     for(i=1;i<=n;i++)
+       {
+    
+                 System.out.println(a);      
+                 sum=a+b;
+                 a=b;
+                 b=sum;
+       } 
+                 
+   }
+ }   
+      
+    
+    
